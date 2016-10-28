abfd4a2ac45f948bb4327840832f4faa90ca6af3
dudeguy <3403>
2016-10-28 07:10:47 -0400

    auto-commit hackybook October 28, 2016 7:07 AMauto-commit hackybook October 28, 2016 7:10 AM

--- Pinball Attack/GameScene.swift
+++ Pinball Attack/GameScene.swift
@@ -16,29 +16,25 @@ var gView:	SKView?
 
 // ***************************** \\
 class Pinball {
-	static let scene: SKScene = gScene!
-	//static let c = Configz.c.b // TODO: Memory issues
 	
 	enum Size: CGFloat {
 		
 		case small, normal, large
 		
-		func toRadius(scene: SKScene = Pinball.scene) -> CGFloat {
-			let c = ConBall()
-			
+		func toRadius(scene: SKScene = gScene!, c: ConBall = ConBall()) -> CGFloat {
 			switch self {
-				case .small:  return (scene.frame.width / c.small ) 		// Config.Ball
-				case .normal:	return (scene.frame.width / c.normal)
-				case .large:  return (scene.frame.width / c.large )
+			case .small:  return (scene.frame.width / c.small )
+			case .normal:	return (scene.frame.width / c.normal)
+			case .large:  return (scene.frame.width / c.large )
 			}
 		}
 	}
-
+	
 	// IA Fields:
 	let radius: CGFloat
-
+	
 	init(size: Pinball.Size) {
-
+		
 		// Sudden death round is ultra-multiball madness!
 		
 		self.radius = size.toRadius()
@@ -47,12 +43,10 @@ class Pinball {
 
 // ***************************** \\
 class Flipper: SKSpriteNode {
-	
 	required init?(coder aDecoder: NSCoder) {		fatalError("")	}
 	static func notes()	{
 		// FLipper size is 1/4 of width
 	}
-	
 	
 	// Enums:
 	enum Flip {
@@ -69,14 +63,8 @@ class Flipper: SKSpriteNode {
 	var power: CGFloat
 	var state: Flip.State
 	
-	// Methods:
-	func flip()	{		self.state = Flip.State.up		/* Do physics stuff */	}
-	
 	// Init:
-	init(side: Flip.Side, player: Flip.Player, ball: Pinball) {
-		
-		let scene = gScene!
-		let c = ConFlip()
+	init(side: Flip.Side, player: Flip.Player, ball: Pinball, scene: SKScene = gScene!, c: ConFlip = ConFlip()) {
 		
 		inits: do {
 			
@@ -91,63 +79,51 @@ class Flipper: SKSpriteNode {
 			scene.addChild(self)
 		}
 		
-		findAnchorAndPos: do {
-			// Find anchorpoint:
-			if self.player == Flip.Player.bottom {
-				switch side {
-				case .left:
-					self.anchorPoint.x = 0
-					self.anchorPoint.y = self.frame.midY
-				case .right:
-					self.anchorPoint.x = 1
-					self.anchorPoint.y = self.frame.midY
-				}
+		setAnchor: do {
+			switch self.side {
+			case .left:
+				self.anchorPoint.x = 0
+				self.anchorPoint.y = self.frame.midY
+			case .right:
+				self.anchorPoint.x = 1
+				self.anchorPoint.y = self.frame.midY
 			}
-			// Find position:
+		}
+		
+		setPositions: do {
 			let gap = (ball.radius * c.gap_factor)
 			
 			if self.player == Flip.Player.bottom {
 				switch side {
 				case .left:
 					self.position.y = (scene.frame.minY + c.offset)
-					self.position.x = scene.frame.midX
-					self.position.x -= (self.frame.width + gap)
+					self.position.x = (scene.frame.midX - (self.frame.width + gap))
 				case .right:
 					self.position.y = (scene.frame.minY + c.offset)
-					self.position.x = scene.frame.midX
-					self.position.x += (self.frame.width + gap)
+					self.position.x = (scene.frame.midX + (self.frame.width + gap))
 				}
 			}
+				
 			else if self.player == Flip.Player.top {
 				switch side {
 				case .left:
 					self.position.y = (scene.frame.maxY - c.offset)
-					self.position.x = scene.frame.midX
-					self.position.x -= (self.frame.width + gap)
+					self.position.x = (scene.frame.midX - (self.frame.width + gap))
 				case .right:
 					self.position.y = (scene.frame.maxY - c.offset)
-					self.position.x = scene.frame.midX
-					self.position.x += (self.frame.width + gap)
+					self.position.x = (scene.frame.midX + (self.frame.width + gap))
 				}
 			}
 		}
-	}
+	}/* /init */
 }
 
 // ***************************** \\
 struct Player {
 	
-	// static let scene = gScene
-	
 	let flipper: (left: Flipper, right: Flipper)
 	
 	var score: Int
-	
-	//	func position(xy: CGPoint) {
-	//		flipper.left.position = xy
-	//			flipper.right.position.x = flipper.left.frame.maxX
-	//			flipper.right.position.y = flipper.left.frame.maxY
-	//	}
 	
 	init(player: Flipper.Flip.Player, ball: Pinball) {
 		self.flipper.left = Flipper(side: .left, player: player, ball: ball)
@@ -188,46 +164,37 @@ class GameScene: SKScene {
 	// ** \\
 	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 		for touch in touches {
-			let view = gView!
-			let scene = gScene!; print(scene)
+			let TLOC = touch.locationInNode(self); print(TLOC)
 			
-			let c = ConPhy()
-			
-			let tloc = touch.locationInNode(self); print(tloc)
-			
-			flipPlayerBottom: do {
-				let sequence_left = SKAction.sequence([
+			func flip(c: ConPhy = ConPhy(), view: SKView = gView!, scene: SKScene = gScene!,
+			          tloc: CGPoint = TLOC, player: (bottom: Player, top: Player) = self.player!) {
+				
+				let pos_neg = SKAction.sequence([
 					SKAction.rotateByAngle(c.dist, duration: c.flip_up),
 					SKAction.rotateByAngle(-c.dist, duration: c.flip_down)])
 				
-				let sequence_right = SKAction.sequence([
+				let neg_pos = SKAction.sequence([
 					SKAction.rotateByAngle(-c.dist, duration: c.flip_up),
 					SKAction.rotateByAngle(c.dist, duration: c.flip_down)])
 				
-				if tloc.x <= view.center.x
-				{	player?.bottom.flipper.left.runAction(sequence_left) }
-				else if tloc.x >= view.center.x
-				{ player?.bottom.flipper.right.runAction(sequence_right) }
-			}
-			
-			flipPlayerTop: do {
-				let sequence_left	= SKAction.sequence([
-					SKAction.rotateByAngle(-c.dist, duration: c.flip_up),
-					SKAction.rotateByAngle(c.dist, duration: c.flip_down)])
+				flipPlayerBottom: do {
+					if (tloc.y <= view.center.y) {
+						(tloc.x <= view.center.x) ?
+							player.bottom.flipper.left.runAction(pos_neg) : // Flip left
+							player.bottom.flipper.right.runAction(neg_pos)  // Flip right
+					}
+				}
 				
-				let sequence_right = SKAction.sequence([
-					SKAction.rotateByAngle(c.dist, duration: c.flip_up),
-					SKAction.rotateByAngle(-c.dist, duration: c.flip_down)])
-				
-				if tloc.x <= view.center.x
-				{	player?.top.flipper.left.runAction(sequence_left) }
-				else if tloc.x >= view.center.x
-				{ player?.top.flipper.right.runAction(sequence_right) }
-				
-			}
+				flipPlayerTop: do {
+					if (tloc.y > view.center.y) {
+						tloc.x <= view.center.x ?
+							player.top.flipper.left.runAction(neg_pos) : // Flip left
+							player.top.flipper.right.runAction(pos_neg)  // Flip right
+					}
+				}
+			}; flip()
 		}
 	}
-	
 	
 	// ** \\
 	override func update(currentTime: CFTimeInterval) {
