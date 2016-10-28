52d3a52f1679211bf033d97b409bf8c5678d9b59
dudeguy <3403>
2016-10-28 09:07:23 -0400

    auto-commit hackybook October 28, 2016 9:07 AMw

--- Pinball Attack/GameScene.swift
+++ Pinball Attack/GameScene.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2016 Dude Guy . All rights reserved.
 //
 
+/// WHY THE FUCK DOES SELF KEEP SCREWING ME OVER????
+
 // MARK: - Top -
 import SpriteKit
 
@@ -14,11 +16,12 @@ var gScene: SKScene?
 var gView:	SKView?
 
 // ***************************** \\
+private var ball: Pinball?
+private var player: (top: Player, bottom: Player)?
+
 class GameScene: SKScene {
 	
 	// ** \\
-	private var player: (top: Player, bottom: Player)? = nil
-	private let ball = Pinball(size: ConBall().default_size)
 	
 	// ** \\
 	override func didMoveToView(view: SKView) {
@@ -35,9 +38,10 @@ class GameScene: SKScene {
 		}
 		
 		
-		player = (bottom:Player(player: .bottom, ball: ball),
-		          top: Player(player: .top, ball: ball))
+		player = (bottom:Player(player: .bottom, ball: ball!),
+		          top: Player(player: .top, ball: ball!))
 		
+		ball = Pinball(size: ConBall().default_size)
 		//player!.top = player!.bottom
 		
 	}
@@ -45,55 +49,55 @@ class GameScene: SKScene {
 	// ** \\
 	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 		
-		for touch in touches {
-			
-			func flip(c: ConPhy = ConPhy(),
-			          view: SKView = gView!,
-			          tloc: CGPoint = touch.locationInNode(self),
-			          player: (bottom: Player, top: Player) = self.player!) {
-				
-				let pos_neg = SKAction.sequence([
-					SKAction.rotateByAngle(c.dist, duration: c.flip_up),
-					SKAction.rotateByAngle(-c.dist, duration: c.flip_down)])
-				
-				let neg_pos = SKAction.sequence([
-					SKAction.rotateByAngle(-c.dist, duration: c.flip_up),
-					SKAction.rotateByAngle(c.dist, duration: c.flip_down)])
-				
-				flipPlayerBottom: do {
-					if (tloc.y <= view.center.y) {
-						(tloc.x <= view.center.x) ?
-							player.bottom.flipper.left.runAction(pos_neg) : // Flip left
-							player.bottom.flipper.right.runAction(neg_pos)  // Flip right
+				for touch in touches {
+		
+					func flip(c: ConPhy = ConPhy(),
+					          view: SKView = gView!,
+					          tloc: CGPoint = touch.locationInNode(self),
+					          player: (bottom: Player, top: Player) = player!) {
+		
+						let pos_neg = SKAction.sequence([
+							SKAction.rotateByAngle(c.dist, duration: c.flip_up),
+							SKAction.rotateByAngle(-c.dist, duration: c.flip_down)])
+		
+						let neg_pos = SKAction.sequence([
+							SKAction.rotateByAngle(-c.dist, duration: c.flip_up),
+							SKAction.rotateByAngle(c.dist, duration: c.flip_down)])
+		
+						flipPlayerBottom: do {
+							if (tloc.y <= view.center.y) {
+								(tloc.x <= view.center.x) ?
+									player.bottom.flipper.left.runAction(pos_neg) : // Flip left
+									player.bottom.flipper.right.runAction(neg_pos)  // Flip right
+							}
+						}
+		
+						flipPlayerTop: do {
+							if (tloc.y > view.center.y) {
+								tloc.x <= view.center.x ?
+									player.top.flipper.left.runAction(neg_pos) : // Flip left
+									player.top.flipper.right.runAction(pos_neg)  // Flip right
+							}
+						}
 					}
+		
+					// FUCKING BUGS:
+					flip(tloc: touch.locationInNode(self))
 				}
-				
-				flipPlayerTop: do {
-					if (tloc.y > view.center.y) {
-						tloc.x <= view.center.x ?
-							player.top.flipper.left.runAction(neg_pos) : // Flip left
-							player.top.flipper.right.runAction(pos_neg)  // Flip right
-					}
-				}
-			}
-			
-			// FUCKING BUGS:
-			flip(tloc: touch.locationInNode(self), player: self.player!)
-		}
 	}
 	
-//	override func update(currentTime: CFTimeInterval){
-//	
-//		resetPinball: do {
-//			let sbnpx = self.ball.node.position.x
-//			let sbnpy = self.ball.node.position.y
-//			
-//			if (sbnpx == self.frame.minX) || (sbnpx == self.frame.maxX) {
-//				self.ball.node.position = self.center
-//			}
-//			else if (sbnpy == self.frame.minY) || (sbnpy == self.frame.maxY) {
-//				self.ball.node.position = self.center
-//			}
-//		}
-//	}
+	override func update(currentTime: CFTimeInterval){
+		
+		resetPinball: do {
+			let sbnpx = ball!.node.position.x
+			let sbnpy =  ball!.node.position.y
+			
+			if (sbnpx == self.frame.minX) || (sbnpx == self.frame.maxX) {
+				ball!.node.position = self.center
+			}
+			else if (sbnpy == self.frame.minY) || (sbnpy == self.frame.maxY) {
+				ball!.node.position = self.center
+			}
+		}
+	}
 }
