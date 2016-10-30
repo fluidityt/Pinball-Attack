f6ae3652ecb30f53caea563199e919b535b0eae5
dudeguy <3403>
2016-10-30 00:17:10 -0400

    auto-commit hackybook October 30, 2016 12:17 AM

--- Pinball Attack/File.swift
+++ Pinball Attack/File.swift
@@ -3,47 +3,44 @@ import UIKit
 
 
 
-var gFullStop = FullStopHandler.full_stop_instance
+var gFullStop = FullStopHandler.full_stop_instance // hi
 
 struct FullStopHandler {
+
+	// Inits:
+	private static var full_stop_instance = FullStopHandler
+	let stop_dict: [SKNode: [CGVector]?]
+	private init() {} // protected
 	typealias this = FullStopHandler;
 
-	private static var full_stop_instance = FullStopHandler
-
-	private init () {} // protected
-
-	let stop_dict: [SKNode: [CGVector]?] = [:]
-
-	var z = "hi"
-	static func queueForce ( next_force: CGVector, to node: SKNode, FP object: FullStopHandler )
+	static func queueForce( next_force: CGVector, to node: SKNode, FP object: FullStopHandler )
 					-> FullStopHandler {
 
 		// Check if empty:
-		if this.stop_dict[node] == nil {
+		if object.stop_dict[node] == nil {
 			var new_dict = object.stop_dict
-			return this ( stop_dict: new_dict.stop_dict.updateValue ( [next_force], forKey: node )
+			return this( stop_dict: new_dict.updateValue( [next_force], forKey: node ) )
 		}
 
 		// Append if not:
 		else {
 			var forceQ = this.stop_dict[node]!
-			forceQ!.append ( next_force )
-			OOP --> this.stop_dict.updateValue ( forceQ!, forKey: node )
+			forceQ!.append( next_force )
+			OOP --> this.stop_dict.updateValue( forceQ!, forKey: node )
 		}
 	} // Called in SKPB
 
-	static func stop ( node: SKNode ) {
+	static func stop( node: SKNode ) {
 		if node.physicsBody?.pinned == false {
 			return
 		}
 		else {
 			node.physicsBody?.pinned = true
-			/* Reset the pinned status on next frame ( .handle() )*/
-			OOP --> this.stop_dict.updateValue ( nil, forKey: node )
+			OOP --> this.stop_dict.updateValue( nil, forKey: node )
 		}
 	} // Called in SKPB
 
-	static func handle () {
+	static func handle() {
 
 		// Unpin, then apply any Qd forces:
 		if this.stop_dict.count > 0 {
@@ -57,11 +54,11 @@ struct FullStopHandler {
 
 					// Apply forces:
 					for force in this.stop_dict[node]!! {
-						node.physicsBody?.applyForce ( force )
+						node.physicsBody?.applyForce( force )
 					}
 				}
 			}
-			OOP --> this.stop_dict.removeAll ()
+			OOP --> this.stop_dict.removeAll()
 		}
 	} // Called in update
 }
