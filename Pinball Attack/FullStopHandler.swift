65c284ec9e623558a1c0ed29985a1a5580c6d73d
dudeguy <3403>
2016-10-30 09:02:38 -0400

    auto-commit hackybook October 30, 2016 9:02 AM

--- Pinball Attack/FullStopHandler.swift
+++ Pinball Attack/FullStopHandler.swift
@@ -9,17 +9,30 @@ var gNodesToCheck = Set<SKNode>()
 /* For use in FullStopHandler: */
 extension SKNode {
 
-	var addToCheck: Bool /*-*/ { get { return self.addToCheck } set {} } /*_*/
-	
-	var nextForces: [CGVector] /*-*/{	get { return self.nextForces} set {} } /*_*/
+	var addToCheck: Bool          /*-*/ { get { return self.addToCheck } set {} } /*_*/
+
+	var nextForces: [CGVector]    /*-*/ {	get { return self.nextForces} set {} } /*_*/
+
+	/** Fileprivate checks self against global and adds if needed: */
+	private func addToChecklist(inout noded: Set<SKNode>) {
+		noded.contains( self ) ? () : noded.insert( self )
+	}
 
 	/** Checks if pinned.. if yes, puts in NextForces */
-	func applyForce( force: CGVector ) {
+	func applyForce( force: CGVector, inout checkList gnodes_to_check: Set<SKNode> ) {
 		/* TODO: Why can't I put this in the fucking default prams? */
+		addToChecklist(&gnodes_to_check)
+		
 		let pb = self.physicsBody!
 		pb.pinned ? self.nextForces.append( force ) : pb.applyForce( force )
 	}
 
+	func stop(inout checkList gnodes_to_check: Set<SKNode>) {
+		addToChecklist(&gnodes_to_check)
+
+		let pb = self.physicsBody!
+		pb.pinned = false
+	}
 }
 
 
