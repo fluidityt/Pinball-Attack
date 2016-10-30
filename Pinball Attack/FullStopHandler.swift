443fbecdf96381240c68f14249e34f4ae5c1d8b6
dudeguy <3403>
2016-10-30 08:13:34 -0400

    auto-commit hackybook October 30, 2016 8:13 AM

--- Pinball Attack/FullStopHandler.swift
+++ Pinball Attack/FullStopHandler.swift
@@ -9,16 +9,19 @@ var gNodesToCheck: [SKNode] = []
 /* For use in FullStopHandler: */
 extension SKNode {
 
-	var nextForces: [CGVector] /*-*/{	get { return nextForces } set {}} /*_*/
+	var nextForces: [CGVector] /*-*/{	get { return self.nextForces} set {}} /*_*/
 
 	// Checks if pinned.. if yes, puts in NextForces
 	func applyForce( force: CGVector ) {
-		let pb = self.physicsBody!
-
+		let pb
+				= self.physicsBody! /* TODO: Why can't I put this in the fucking default prams? */
 		pb.pinned ? self.nextForces.append( force ) : pb.applyForce( force )
-
 	}
 
+	convenience public init( addToNodeCheck: Bool ) {
+		self.init()
+		addToNodeCheck ? gNodesToCheck.append( self ) : ()
+	}
 }
 
 
