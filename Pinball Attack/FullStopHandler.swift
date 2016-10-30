3b9ef6ef5d81544bd49b47f7a13cbb7c563a5107
dudeguy <3403>
2016-10-30 08:38:35 -0400

    auto-commit hackybook October 30, 2016 8:38 AM

--- Pinball Attack/FullStopHandler.swift
+++ Pinball Attack/FullStopHandler.swift
@@ -4,24 +4,22 @@ import UIKit
 
 
 // All potential nodes that could need stop / start or Qing go in here
-var gNodesToCheck: [SKNode] = []
+var gNodesToCheck = Set<SKNode>()
 
 /* For use in FullStopHandler: */
 extension SKNode {
 
-	var nextForces: [CGVector] /*-*/{	get { return self.nextForces} set {}} /*_*/
+	var addToCheck: Bool /*-*/ { get { return self.addToCheck } set {} } /*_*/
+	
+	var nextForces: [CGVector] /*-*/{	get { return self.nextForces} set {} } /*_*/
 
-	// Checks if pinned.. if yes, puts in NextForces
+	/** Checks if pinned.. if yes, puts in NextForces */
 	func applyForce( force: CGVector ) {
-		let pb
-				= self.physicsBody! /* TODO: Why can't I put this in the fucking default prams? */
+		/* TODO: Why can't I put this in the fucking default prams? */
+		let pb = self.physicsBody!
 		pb.pinned ? self.nextForces.append( force ) : pb.applyForce( force )
 	}
 
-	convenience public init( addToNodeCheck: Bool ) {
-		self.init()
-		addToNodeCheck ? gNodesToCheck.append( self ) : ()
-	}
 }
 
 
