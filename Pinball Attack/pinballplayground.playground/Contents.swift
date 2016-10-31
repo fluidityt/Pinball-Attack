a19ab8793c844ce235fd29dff0e11031d6e01a02
dudeguy <3403>
2016-10-31 03:05:48 -0400

    auto-commit hackybook October 31, 2016 3:05 AM

--- Pinball Attack/pinballplayground.playground/Contents.swift
+++ Pinball Attack/pinballplayground.playground/Contents.swift
@@ -7,7 +7,7 @@ struct Cat {
 	
 	// Ooper convenience:
 	var lastCat: Any = Cat(age: 3, name: "")
-	mutating func islastCat(lc: Cat) { lastCat = lc }
+	mutating func islastCat() { lastCat = self }
 	
 	// Field:
 	let
@@ -15,22 +15,26 @@ struct Cat {
 	name: String,
 	num_rockets: Int
 	
-	
+	// Meth:
+	func fireRocket() -> Cat {
+		return Cat(fromOldCat: self, num_rockets: (self.num_rockets - 1))
+	}
+		
 	
 	// Init:
 	init(age: Int, name: String) {
 		self.age = age
 		self.name = name
 		num_rockets = 5
+		
 	}
 	
 	// FP transformation:
-	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil) {
+	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, num_rockets: Int? = nil) {
 		age ==  nil ? (self.age = oc.age)   : (self.age = age!)
 		name == nil ? (self.name = oc.name) : (self.name = name!)
-		
-		num_rockets = oc.num_rockets
-		//self.islastCat(<#T##lc: Cat##Cat#>)
+		num_rockets == nil ? (self.num_rockets = oc.num_rockets) : (self.num_rockets = num_rockets!)
+	
 	}
 	
 }
