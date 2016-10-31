d14613945801241019f2ffec75e0483a899c150b
dudeguy <3403>
2016-10-31 04:13:51 -0400

    auto-commit hackybook October 31, 2016 4:13 AM

--- Pinball Attack/pinballplayground.playground/Contents.swift
+++ Pinball Attack/pinballplayground.playground/Contents.swift
@@ -1,40 +1,51 @@
 //: Playground - noun: a place where people can play
 
+var global: Any
+
 private let globalName = "Flufferton"
+let fps: Any = ()
+typealias FPS = Any
+
+prefix operator <*{}
+prefix func <*<T>(inout l: T) {  }
+
+postfix operator *>{}
+postfix func *><T>(r: T) -> T {	return r }
+
+infix operator ->>{}
+func ->><T>(inout l: T, r: T){
+	l = r
+}
 
 /// Says meow:
 struct Cat {
-	
-	// Ooper convenience:
-	var lastCat: Any = Cat(age: 3, name: "")
-	mutating func islastCat() { lastCat = self }
-	
+
 	// Field:
 	let
 	age: Int,
 	name: String,
-	num_rockets: Int
+	rockets: Int, // Our kitty is srsbznz... been wraned..
+	lives: Int
 	
 	// Meth:
-	func fireRocket() -> Cat {
-		return Cat(fromOldCat: self, num_rockets: (self.num_rockets - 1))
-	}
+	func fireRocket() -> Cat {return Cat(fromOldCat: self, rockets: (self.rockets - 1))}
+	func takeDamage() -> Cat {return Cat(fromOldCat: self, lives: (self.lives - 1))}
 		
-	
 	// Init:
 	init(age: Int, name: String) {
 		self.age = age
 		self.name = name
-		num_rockets = 5
+		rockets = 5
+		lives = 9
 		
 	}
 	
 	// FP transformation:
-	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, num_rockets: Int? = nil) {
+	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, rockets: Int? = nil, lives: Int? = nil) {
 		age ==  nil ? (self.age = oc.age)   : (self.age = age!)
 		name == nil ? (self.name = oc.name) : (self.name = name!)
-		num_rockets == nil ? (self.num_rockets = oc.num_rockets) : (self.num_rockets = num_rockets!)
-	
+		rockets == nil ? (self.rockets = oc.rockets) : (self.rockets = rockets!)
+		lives == nil ? (self.lives = oc.lives) : (self.lives = lives!)
 	}
 	
 }
@@ -42,10 +53,15 @@ struct Cat {
 // Testing:
 private let fluffy = Cat(age: 4, name: "Fluffy")
 
-let kitty = Cat(fromOldCat: fluffy, age: 34)
+let kitty = Cat(fromOldCat: fluffy, age: 34, name: "Kitty")
 
 print(kitty)
 print(fluffy)
 
+var boots = Cat(fromOldCat: kitty, name: "Boots")
+boots ->> boots.fireRocket()
+boots ->> boots.takeDamage()
+
+print(boots)
 
 
