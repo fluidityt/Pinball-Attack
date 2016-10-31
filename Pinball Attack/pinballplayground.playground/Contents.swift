ff9d21b1340142b572bf82a3583588f232fc7c94
dudeguy <3403>
2016-10-31 04:36:45 -0400

    auto-commit hackybook October 31, 2016 4:36 AM

--- Pinball Attack/pinballplayground.playground/Contents.swift
+++ Pinball Attack/pinballplayground.playground/Contents.swift
@@ -28,12 +28,15 @@ struct Cat {
 	lives: Int,
 	
 	// Battle!!
+	
 	damage_to_give: Int,
 	damage_to_take: Int,
-	damage_left: Int
+	damage_left: Int,
+	status: String
 	
 	let AP = 40
 	let DEF = 20
+	let MAXHP = 50
 	
 	// Meth:
 	func fireRocket(at target_cat: Cat) -> Cat {
@@ -43,12 +46,31 @@ struct Cat {
 		return Cat(fromOldCat: self, rockets: rock_ets, damage_to_give: damtogive)
 	}
 	func takeDamage(from that_cat: Cat) -> Cat {
-		let
-		return Cat(fromOldCat: self, lives: (self.lives - 1))
+		let damtaken = that_cat.damage_to_give
+		let damleft = (self.damage_left - damtaken)
+		
+		// returners:
+		let damleft2: Int
+		let li_ves: Int
+		let sta_tus: String
+		
+		if damleft <= 0 {
+			damleft2 = self.MAXHP
+			li_ves = (self.lives - 1)
+			li_ves == 0 ? (sta_tus = "Dead") : (sta_tus = "Alive")
+		}
+			
+		else {
+			damleft2 = damleft
+			li_ves = self.lives
+			sta_tus = "Alive"
+		}
+		
+		return Cat(fromOldCat: self, lives: li_ves, damage_left: damleft2, status: sta_tus)
 	}
 	
 	// FP transformation:
-	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, rockets: Int? = nil, lives: Int? = nil, damage_to_give: Int? = nil, damage_to_take: Int? = nil, damage_left: Int? = nil) {
+	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, rockets: Int? = nil, lives: Int? = nil, damage_to_give: Int? = nil, damage_to_take: Int? = nil, damage_left: Int? = nil, status: String? = nil) {
 		
 		// Basics:
 		age ==  nil ? (self.age = oc.age)   : (self.age = age!)
@@ -58,23 +80,22 @@ struct Cat {
 		
 		// Battle:
 		if damage_to_give == nil { self.damage_to_give = oc.damage_to_give}
-		else { self.damage_to_give = damage_to_give! }
-		
+		 else { self.damage_to_give = damage_to_give! }
 		if damage_to_take == nil { self.damage_to_give = oc.damage_to_take }
-		else { self.damage_to_take = oc.damage_to_take }
-		
+		 else { self.damage_to_take = oc.damage_to_take }
 		damage_left == nil ? (self.damage_left = oc.damage_left) : (self.damage_left = damage_left!)
+		status == nil ? (self.status = oc.status) : (self.status = status!)
 	}
 	
 }
 
 // Testing:
-private let fluffy = Cat()
-
-let kitty = Cat(fromOldCat: fluffy, age: 34, name: "Kitty")
 
 print(kitty)
 print(fluffy)
+
+var kitty = Cat(fromOldCat: fluffy, age: 34, name: "Kitty")
+
 
 var boots = Cat(fromOldCat: kitty, name: "Boots")
 
