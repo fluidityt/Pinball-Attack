6af55e153c2e9c613e8fc23c20733cfb63bdaf46
dudeguy <3403>
2016-10-31 04:20:14 -0400

    auto-commit hackybook October 31, 2016 4:20 AM

--- Pinball Attack/pinballplayground.playground/Contents.swift
+++ Pinball Attack/pinballplayground.playground/Contents.swift
@@ -25,33 +25,38 @@ struct Cat {
 	age: Int,
 	name: String,
 	rockets: Int, // Our kitty is srsbznz... been wraned..
-	lives: Int
+	lives: Int,
+	
+	// Battle!!
+	damage_to_give: Int,
+	damage_to_take: Int
+	
 	
 	// Meth:
 	func fireRocket() -> Cat {return Cat(fromOldCat: self, rockets: (self.rockets - 1))}
 	func takeDamage() -> Cat {return Cat(fromOldCat: self, lives: (self.lives - 1))}
-		
-	// Init:
-	init(age: Int, name: String) {
-		self.age = age
-		self.name = name
-		rockets = 5
-		lives = 9
-		
-	}
 	
 	// FP transformation:
-	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, rockets: Int? = nil, lives: Int? = nil) {
+	init(fromOldCat oc: Cat, age: Int? = nil, name: String? = nil, rockets: Int? = nil, lives: Int? = nil, damage_to_give: Int? = nil, damage_to_take: Int? = nil) {
+		
+		// Basics:
 		age ==  nil ? (self.age = oc.age)   : (self.age = age!)
 		name == nil ? (self.name = oc.name) : (self.name = name!)
 		rockets == nil ? (self.rockets = oc.rockets) : (self.rockets = rockets!)
 		lives == nil ? (self.lives = oc.lives) : (self.lives = lives!)
+		
+		// Battle:
+		if damage_to_give == nil { self.damage_to_give = oc.damage_to_give}
+		else { self.damage_to_give = damage_to_give! }
+		
+		if damage_to_take == nil { self.damage_to_give = oc.damage_to_take }
+		else { self.damage_to_take = oc.damage_to_take }
 	}
 	
 }
 
 // Testing:
-private let fluffy = Cat(age: 4, name: "Fluffy")
+private let fluffy = Cat()
 
 let kitty = Cat(fromOldCat: fluffy, age: 34, name: "Kitty")
 
@@ -59,7 +64,9 @@ print(kitty)
 print(fluffy)
 
 var boots = Cat(fromOldCat: kitty, name: "Boots")
+
 boots ->> boots.fireRocket()
+
 boots ->> boots.takeDamage()
 
 print(boots)
