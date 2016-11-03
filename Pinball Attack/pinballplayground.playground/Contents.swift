0d16ebebf7f57c006a467c955b833971453e64fa
dudeguy <3403>
2016-11-03 09:33:32 -0400

    auto-commit hackybook November 3, 2016 9:33 AM

--- Pinball Attack/pinballplayground.playground/Contents.swift
+++ Pinball Attack/pinballplayground.playground/Contents.swift
@@ -25,50 +25,6 @@ struct Cat {
 	let MAXHP = 50
 	
 	
-	/* Methods: */
-	
-	/// Calculates damage to give (from $0's DEF), then stores it in own field:
-	func fireRocket(at victim: Cat) -> Cat {
-		let dmg_to_give2 = (self.AP - victim.DEF)
-		let rockets2 = (self.rockets - 1)
-		
-		return Cat(fromOldCat: self, rockets: rockets2, dmg_to_give: dmg_to_give2)
-	}
-	
-	/// Decreases own HP from value stored in other cat, then updates
-	func takeDamage(from attacker: Cat) -> Cat {
-		
-		// Returners:
-		let dmg_left2: Int
-		let lives2: Int
-		let status2: String
-
-		assignmentLogic: do {
-			
-			let dmg_taken = attacker.dmg_to_give
-			let dam_left = (self.hp - dmg_taken)
-			
-			if dam_left <= 0 {
-				dmg_left2 = self.MAXHP
-				lives2 = (self.lives - 1)
-				lives2 == 0 ? (status2 = "Dead") : (status2 = "Alive")
-			}
-				
-			else {
-				dmg_left2 = dam_left
-				lives2 = self.lives
-				status2 = "Alive"
-			}
-		}
-		
-		return Cat(fromOldCat: self, lives: lives2, dmg_left: dmg_left2, status: status2)
-	}
-	
-	func resetDamages() -> Cat {
-		return Cat(fromOldCat: self, dmg_to_give: 0, hp: 0)
-	}
-	
-	
 	/* Inits: */
 	
 	/// Default init:
@@ -96,6 +52,53 @@ struct Cat {
 		hp == nil 	 	 ?  (self.hp = oc.hp) 					:(self.hp = hp!)
 		dmg_to_give == nil ? (self.dmg_to_give = oc.dmg_to_give):(self.dmg_to_give = dmg_to_give!)
 	}
+	
+	
+	/* Methods: */
+	
+	/// Calculates damage to give (from $0's DEF), then stores it in own field:
+	func fireRocket(at victim: Cat) -> Cat {
+		let dmg_to_give2 = (self.AP - victim.DEF)
+		let rockets2 = (self.rockets - 1)
+		
+		return Cat(fromOldCat: self, rockets: rockets2, dmg_to_give: dmg_to_give2)
+	}
+	
+	/// Decreases own HP from value stored in other cat, then updates
+	func takeDamage(from attacker: Cat) -> Cat {
+		
+		// Returners:
+		let hp2: Int
+		let lives2: Int
+		let status2: String
+
+		assignmentLogic: do {
+			
+			let dmg_taken = attacker.dmg_to_give
+			let dam_left = (self.hp - dmg_taken)
+			
+			if dam_left <= 0 {
+				hp2 = self.MAXHP
+				lives2 = (self.lives - 1)
+				lives2 == 0 ? (status2 = "Dead") : (status2 = "Alive")
+			}
+				
+			else {
+				hp2 = dam_left
+				lives2 = self.lives
+				status2 = "Alive"
+			}
+		}
+		
+		return Cat(fromOldCat: self, hp: hp2, lives: lives2, status: status2 )
+	}
+	
+	func resetDamages() -> Cat {
+		return Cat(fromOldCat: self, dmg_to_give: 0, hp: 0)
+	}
+	
+	
+
 } //</cat>
 
 
@@ -142,79 +145,91 @@ var gCatList = CatList(defaults: 1)
 
 /* Combat funk */
 
-/// 1v1 Combatants:
-typealias Combatants = (attacker: Cat, victim: Cat)
-
-/// Makes doCombat (defined next) more Englishy / fun, by pairing an enum to a 1v1 Cat.method()
-enum Attacks {
+/// Static / abstract struct (func only):
+struct Battle {
 	
-	case fires1Rocket
+	/// 1v1 Combatants:
+	typealias Combatants = (attacker: Cat, victim: Cat)
 	
-	func becomeFunc(combatants: Combatants) -> ()->(Cat) {
-		let attacker = combatants.attacker
-		let victim  = combatants.victim
-		switch self {
+	/// Makes doCombat (defined next) more Englishy / fun, by pairing an enum to a 1v1 Cat.method()
+	enum Attacks {
+		
+		case fires1Rocket
+		
+		func becomeFunc(combatants: Combatants) -> ()->(Cat) {
+			let attacker = combatants.attacker
+			let victim  = combatants.victim
+			switch self {
 			case fires1Rocket:
 				return { attacker.fireRocket( at: victim) }
+			}
 		}
 	}
-}
-
-
-/// Returns two new cats after battling.. Use them to update your CatList:
-func doCombat(attacker: Cat, _ const: Attacks, at victim: Cat) -> Combatants {
 	
-	// New funcs that make a new Cat each:
-	let attacked = const.becomeFunc((attacker, victim))
-	let victimized = { victim.takeDamage(from: attacked()) }
 	
-	return (attacker: attacked(), victim: victimized())
-}
-
-/// Mutates our gCatList automagically with the battle results:
-func handleResults(list: CatList = gCatList,
-                   cats_after_btl: Combatants)
- -> CatList {
-	
-
-	func matchName(combatant: (name: String, is_attacker: Bool),
-	               list2UpdateFrom: CatList)
-		-> CatList {
-			
-			// Returner:
-			let new_cat: Cat
-			
-			// Logic1:
-			combatant.is_attacker ?
-				(new_cat = cats_after_btl.attacker) : (new_cat = cats_after_btl.victim)
-			
-			// Logic2:
-			typealias n=CatList.Names
-			switch combatant.name {
-				case n.boots:  return CatList (ol: list2UpdateFrom, boots: new_cat)
-				case n.fluffy: return CatList (ol: list2UpdateFrom, fluffy: new_cat)
-				case n.kitty:  return CatList (ol: list2UpdateFrom, kitty: new_cat)
-				
-				// Induce crash:
-				default: print("error cant find cat"); let error: CatList? = nil; return error!
-			}
+	/// Returns two new cats after battling.. Use them to update your CatList:
+	static func doCombat(attacker: Cat, _ const: Attacks, at victim: Cat) -> Combatants {
+		
+		// New funcs that make a new Cat each:
+		let attacked = const.becomeFunc((attacker, victim))
+		let victimized = { victim.takeDamage(from: attacked()) }
+		
+		return (attacker: attacked(), victim: victimized())
 	}
 	
-	//	matchName(results.attacker.name, is_attacker: true, list2: list).boots.damage_to_give
-	let attacker2 = (cats_after_btl.attacker.name, true)
-	let victim2 = (cats_after_btl.victim.name, false)
+	/// Mutates our gCatList automagically with the battle results:
+	static func handleResults(battled_cats battled_cats: Combatants, fromInitial list: CatList)	-> CatList {
+		
+		// Returner method:
+		func matchName(this_cat: (name: String, is_attacker: Bool),
+		               updateFrom list2: CatList)
+			-> CatList {
+				
+				// Returner:
+				let new_cat: Cat
+				
+				// Logic1:
+				this_cat.is_attacker ?
+					(new_cat = battled_cats.attacker) : (new_cat = battled_cats.victim)
+				
+				// Logic2:
+				typealias n=CatList.Names
+				switch this_cat.name {
+					case n.boots:  return CatList (ol: list2, boots: new_cat)
+					case n.fluffy: return CatList (ol: list2, fluffy: new_cat)
+					case n.kitty:  return CatList (ol: list2, kitty: new_cat)
+						
+					// Induce crash:
+					default: print("error cant find cat"); let error: CatList? = nil; return error!
+				}
+		}
+		
+		//	matchName(results.attacker.name, is_attacker: true, list2: list).boots.damage_to_give
+		let attacker2 = (name: battled_cats.attacker.name, is_attacker: true)
+		let victim2 	=	(name: battled_cats.victim.name, 	 is_attacker: false)
+		
+		// Attacker must present dmg_to_give to victim (takeDamage())--attacker must be processed first (on the right)
+		return matchName( victim2, updateFrom: (matchName(attacker2, updateFrom: list)) )
+	}
 	
-	return matchName(victim2, list2UpdateFrom: (matchName(attacker2, list2UpdateFrom: list )))
+	/// Brings it all together (in reverse order): start(results = handleResults(cats = matchName(name = doCombat)
+	static func start(combatants: Combatants, initialList gcat_list: CatList = gCatList) -> CatList {
+		let attacker = combatants.attacker
+		let victim = combatants.victim
+		return handleResults(battled_cats: doCombat(attacker, .fires1Rocket, at: victim), fromInitial: gcat_list)
+		
+	}
 }
 
 // MARK: TESTING 2:
 aBattleTest: do {
 	let test_attacker = gCatList.boots
 	let test_victim = gCatList.fluffy
+	let between_combatants = (test_attacker, test_victim)
 	
-	gCatList = handleResults(cats_after_btl: doCombat(test_attacker, .fires1Rocket, at: test_victim))
+	gCatList = Battle.start(between_combatants)
 	
-	print(test_victim.hp)
+	print(gCatList.fluffy.hp)
 }
 
 
